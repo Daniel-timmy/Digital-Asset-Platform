753f0f72b3c65a322ceddc090b36ea007309d230
Daniel-Timmy <ajayitimmy45@gmail.com>
2025-08-05 16:02:33 +0100

    Getting ready to deploy the backend

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -10,6 +10,7 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_USERNAME = process.env.DB_USERNAME;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_NAME = process.env.DB_NAME;
+export const DB_TYPE = process.env.DB_TYPE ; 
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,12 +1,13 @@
 import { DataSource } from "typeorm";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME, DB_TYPE } from "../config/env";
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
+  type: DB_TYPE || "postgres" as any, // Default to postgres if not set
+  host: DB_HOST,
   port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "baselink_asset",
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   synchronize: true, // Set to false in production
   logging: false,
   entities: ["src/entities/**/*.ts"],
