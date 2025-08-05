47f6083ccaf2a860313a3cd96c2bdd36c04808bd
Daniel-Timmy <ajayitimmy45@gmail.com>
2025-08-05 14:54:07 +0100

    Renaming services

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -109,7 +109,7 @@ export default function Example() {
                   onClick={toggleServices}
                   className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium transition"
                 >
-                  Services
+                  SERVICES
                 </button>
                 {servicesOpen && (
                   <div className="absolute mt-2 w-40 bg-gray-800 rounded-md shadow-lg z-50">
